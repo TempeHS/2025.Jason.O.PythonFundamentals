300d2c655c1e348ef1f3e4eecb855b96f38051a4
Jason O'Toole <124547316+MrBananaCrepe@users.noreply.github.com>
2024-02-12 06:22:55 +0000

    working on problem 1-5

---
+++ 1-Conditionals/meal/meal.py
@@ -0,0 +1,16 @@
+def main():
+    time = input("What time is it? ")
+
+def convert(time):
+    (
+        x,
+        y,
+    ) = time.split(":")
+    h = float(x)
+    m = float(60 / y)
+
+
+if h += "__main__":
+    
+
+main()
