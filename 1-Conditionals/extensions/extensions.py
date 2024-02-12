a830be33f9e828ef80dc24a2dc851c3204733113
Jason O'Toole <124547316+MrBananaCrepe@users.noreply.github.com>
2024-02-12 01:04:32 +0000

    Completed problem 1-3

---
+++ 1-Conditionals/extensions/extensions.py
@@ -0,0 +1,23 @@
+def main():
+    file = input("Enter a file: ")
+    file = file.lower().strip("")
+
+    if file.endswith(".gif"):
+        print("image/gif")
+    elif file.endswith(".jpg"):
+        print("image/jpeg")
+    elif file.endswith(".jpeg"):
+        print("image/jpeg")
+    elif file.endswith(".png"):
+        print("image/png")
+    elif file.endswith(".pdf"):
+        print("application/pdf")
+    elif file.endswith(".txt"):
+        print("text/plain")
+    elif file.endswith(".zip"):
+        print("application/zip")
+    else:
+        print("application/octet-stream")
+
+
+main()
