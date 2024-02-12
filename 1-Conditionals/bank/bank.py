d021034eeb4b18edbcdbb6153949a351eef67a10
Jason O'Toole <124547316+MrBananaCrepe@users.noreply.github.com>
2024-02-12 00:45:32 +0000

    Completed problem 1-2

--- 1-Conditionals/bank/bank.py
+++ 1-Conditionals/bank/bank.py
@@ -1,1 +1,12 @@
-greet = input("How would your greet someone? ")
+def main():
+    greet = input("How would your greet someone? ")
+    greet = greet.lower().lstrip(" ")
+    if greet.startswith("hello"):
+        print("$0")
+    elif greet.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+
+main()
