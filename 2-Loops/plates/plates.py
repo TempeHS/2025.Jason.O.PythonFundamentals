3bf6bae9eba2f9c0f6e2b779354843ba16eba72e
Jason O'Toole <124547316+MrBananaCrepe@users.noreply.github.com>
2024-02-18 04:37:32 +0000

    Completed problem 3

---
+++ 2-Loops/plates/plates.py
@@ -0,0 +1,18 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if len(s) < 2 or len(s) > 6:
+        return False
+    if not s.isalpha() and s.isalnum():
+        return False
+    else:
+        return True
+
+
+main()
